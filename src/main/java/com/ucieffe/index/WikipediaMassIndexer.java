5cf2770242fc8a366f763c222feff9de6828fa24
Sanne Grinovero <sanne@hibernate.org>
2011-02-02 11:50:32 +0000

    customizing progress monitor: less verbose

--- src/main/java/com/ucieffe/index/WikipediaMassIndexer.java
+++ src/main/java/com/ucieffe/index/WikipediaMassIndexer.java
@@ -8,6 +8,8 @@ import javax.persistence.Persistence;
 
 import org.hibernate.CacheMode;
 import org.hibernate.search.MassIndexer;
+import org.hibernate.search.batchindexing.MassIndexerProgressMonitor;
+import org.hibernate.search.impl.SimpleIndexingProgressMonitor;
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.impl.FullTextEntityManagerImpl;
 
@@ -17,33 +19,29 @@ public class WikipediaMassIndexer {
 
 	public static void main(String[] args) {
 
-		EntityManagerFactory entityManagerFactory = Persistence
-				.createEntityManagerFactory("wikipedia");
-		EntityManager entityManager = entityManagerFactory
-				.createEntityManager();
-		// Text text = entityManager.find(Text.class, 35422446);
-		// System.out.println(text.getOldText());
-		// System.out.println("Start date:" + new Date());
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory( "wikipedia" );
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		FullTextEntityManager ftEntityManager = new FullTextEntityManagerImpl( entityManager );
 
-		FullTextEntityManager ftEntityManager = new FullTextEntityManagerImpl(
-				entityManager);
-		MassIndexer massIndexer = ftEntityManager.createIndexer(Text.class);
+		MassIndexerProgressMonitor monitor = new SimpleIndexingProgressMonitor( 500 );
+		MassIndexer massIndexer = ftEntityManager.createIndexer( Text.class );
 		try {
 			massIndexer
-					.purgeAllOnStart(true)
-					.optimizeAfterPurge(true)
-					.optimizeOnFinish(true)
-					.batchSizeToLoadObjects(30)
-					.threadsForSubsequentFetching(4)
-					.threadsToLoadObjects(8)
+					.purgeAllOnStart( true )
+					.optimizeAfterPurge( true )
+					.optimizeOnFinish( true )
+					.batchSizeToLoadObjects( 25 )
+					.threadsForSubsequentFetching( 2 )
+					.threadsToLoadObjects( 6 )
 					.threadsForIndexWriter( 6 )
-					.cacheMode(CacheMode.IGNORE)
+					.progressMonitor( monitor )
+					.cacheMode( CacheMode.IGNORE )
 					.startAndWait();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			System.out.println("Stop date:" + new Date());
+			System.out.println( "Ended at: " + new Date() );
 		}
 	}
 }
